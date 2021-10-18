7875d9b28262f15f89cb459468d7be8a36d67637
anilinel <anilinel37@gmail.com>
2021-10-19 00:06:52 +0300

    18ekim ders sonu

---
+++ src/app.js
@@ -0,0 +1,9 @@
+let krediler = ["Konut Kredisi","Araba Kredisi","Evlenme Kredisi"]
+
+console.log(krediler);
+
+console.log("<ul>")
+for( let i=0;i<krediler.length;i++){
+    console.log("<li>"+krediler[i]+"</li>")
+}
+console.log("</ul>")
